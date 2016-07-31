a02a0aaf7028d46d57cb5e6166a393084b1f41a0
ibowen <berwin.lew@gmail.com>
2016-07-31 12:19:01 -0700

    Completed task 3 q-learning

--- smartcab/smartcab/agent3.py
+++ smartcab/smartcab/agent3.py
@@ -3,6 +3,43 @@ from environment import Agent, Environment
 from planner import RoutePlanner
 from simulator import Simulator
 
+class QLearn:
+	def __init__(self, actions, epsilon=0.1, alpha=0.2, gamma=0.9):
+		self.q = {}
+
+		self.epsilon = epsilon
+		self.alpha = alpha
+		self.gamma = gamma
+		self.actions = actions
+
+	def getQ(self, state, action):
+		return self.q.get((state, action), 0.0)
+
+	def updateQ(self, curr_state, action, reward, next_state):
+		newv = reward + self.gamma * max([self.getQ(next_state, a) for a in self.actions])
+		
+		oldv = self.q.get((curr_state, action), None)
+		if oldv is None:
+			self.q[(curr_state, action)] = reward
+		else:
+			self.q[(curr_state, action)] = oldv + self.alpha * (newv - oldv)
+
+	def chooseAction(self, state):
+		if random.random() < self.epsilon:
+			action = random.choice(self.actions)
+		else:
+			q = [self.getQ(state, a) for a in self.actions]
+			maxQ = max(q)
+			count = q.count(maxQ)
+			if count > 1:
+				best = [i for i in range(len(self.actions)) if q[i] == maxQ]
+				i = random.choice(best)
+			else:
+				i = q.index(maxQ)
+
+			action = self.actions[i]
+		return action
+
 class LearningAgent(Agent):
     """An agent that learns to drive in the smartcab world."""
 
@@ -11,6 +48,7 @@ class LearningAgent(Agent):
         self.color = 'red'  # override color
         self.planner = RoutePlanner(self.env, self)  # simple route planner to get next_waypoint
         # TODO: Initialize any additional variables here
+        self.qlearn = QLearn(Environment.valid_actions, epsilon=.05, alpha=0.1, gamma=0.9)
 
     def reset(self, destination=None):
         self.planner.route_to(destination)
@@ -21,28 +59,29 @@ class LearningAgent(Agent):
         self.next_waypoint = self.planner.next_waypoint()  # from route planner, also displayed by simulator
         inputs = self.env.sense(self)
         deadline = self.env.get_deadline(self)
+        
+		# Generate the current state
+        curr_state = inputs.items()
+        curr_state.extend(self.next_waypoint)
+        curr_state = tuple(curr_state)
 
-        # TODO: Update state
-        # Update action based on US traffic rules at intersection
-        action_okay = True
-        if self.next_waypoint == 'right':
-            if inputs['light'] == 'red' and inputs['left'] == 'forward':
-                action_okay = False
-        elif self.next_waypoint == 'forward':
-            if inputs['light'] == 'red':
-                action_okay = False
-        elif self.next_waypoint == 'left':
-            if inputs['light'] == 'red' or (inputs['oncoming'] == 'forward' or inputs['oncoming'] == 'right'):
-                action_okay = False
-		# initialize one action
-        action = None
-        if action_okay:
-            action = self.next_waypoint
+        # Choose the current action
+        action = self.qlearn.chooseAction(curr_state)
             
         # Execute action and get reward
         reward = self.env.act(self, action)
-
-        # TODO: Learn policy based on state, action, reward
+        
+        # Next state
+        # Get the next input of the current state
+        inputs2 = self.env.sense(self)
+        # Get the next waypoint of the next state
+        next2_waypoint = self.planner.next_waypoint()
+        next_state = inputs2.items()
+        next_state.append(next2_waypoint)
+        next_state = tuple(next_state)
+        
+        # Update the current state
+        self.qlearn.updateQ(curr_state, action, reward, next_state)
 
         #print "LearningAgent.update(): deadline = {}, inputs = {}, action = {}, reward = {}".format(deadline, inputs, action, reward)  # [debug]
 
