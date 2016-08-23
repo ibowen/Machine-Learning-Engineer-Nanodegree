c92ee2f1411437f969e0a1bbaf6df33325705ef9
ibowen <berwin.lew@gmail.com>
2016-08-23 13:26:55 -0700

    fix: improve question 3
    add double-edge adjacent list
    add 0-lenght dict input check

--- coding_project/question3.py
+++ coding_project/question3.py
@@ -13,11 +13,13 @@ The vertice with the least weight will be in the head of the queue and be stored
 Meanwhile, the edge of the head vertice is pushed into the queue. 
 The iteration stops when the output dict equals the input adjacent list in length.
 
-Complexity: 
+Time complexity: 
 	worst case: O(nlog(n)), to connect n vertices need n - 1 sorting. And the priority queue insert costs log(n-1), so totally O(nlogn)
 	best case: O(n), only if vertices are along one line.
 	average: O(nlog(n))
 
+Space complexity:
+	mst dictionary is n(vertices), and a priority queue is maximum at total edges m. So the space needs O(n + m)
 """	
 
 from Queue import PriorityQueue
@@ -28,7 +30,7 @@ def question3(G):
 	Parameters: G: a dictionary containing adjacency lists
 	Returns: mst, a dictionary containing adjacency lists with the least weight
 	"""
-	if G is None:
+	if G is None or len(G) < 1:
 		return
 
 	mst = {} # initiate a minimum spanning tree
@@ -37,24 +39,27 @@ def question3(G):
 	vertice, edge_list = G.popitem() # pop one item to start
 	for e in edge_list: # break the first adjacent list into the format "(priority, vetice, edge)"
 		queue.put((0 + e[1], vertice, e))
+
+	G.pop(vertice, None) # pop out the vertice that is done
 	
-	while len(mst) <= len(G) and not queue.empty():
+	while len(G) > 0 and not queue.empty(): # stop when all vertices in G are searched and queue is empty
 		head = queue.get() # dequeue the head, the same as "(priority, vetice, edge)"
-		if head[1] in mst:
-			continue
-		mst[head[1]] = [head[2]] # put head into mst, note: the head is always the adjacent list with the least weight
+		# if head[1] in mst and mst[head[1]] is not None:
+		# 	mst[head[1]] = mst[head[1]].append(head[2]) 
+		# else:
+		if head[1] not in mst:
+			mst[head[1]] = [head[2]] # put head into mst, note: the head is always the adjacent list with the least weight
+		else:
+			mst[head[1]] = list(set(mst[head[1]] + [head[2]]))
 
 		# put the edge of the head into the queue
 		next_vertice = head[2][0]
-		if next_vertice in mst: # if the vertice in the edge already in mst, continue
-			continue
-		for edge in G[next_vertice]:
-				if edge[0] in mst: # if vertice in the edge already in mst, continue
-					continue
-
-				# construct the new adjacent list and put into the queue
-				priority = head[0] + edge[1]
-				queue.put((priority, next_vertice, edge))
+		if next_vertice in G: 
+			for edge in G[next_vertice]:
+					# construct the new adjacent list and put into the queue
+					priority = head[0] + edge[1]
+					queue.put((priority, next_vertice, edge))
+			G.pop(next_vertice, None) # pop out the vertice that is done
 
 	return mst
 
@@ -65,13 +70,17 @@ def main():
 		 'B': [('A', 2)]}
 	print question3(G)
 
-	# should print {'A': [('B', 2)], 'B': [('C', 5)]}
+	# should print {'A': [('B', 2)], 'B': [('C', 5), ('A', 2)]}
 	G = {'A': [('B', 2)],
 		 'B': [('A', 2), ('C', 5)], 
 		 'C': [('B', 5)]}
 	print question3(G)
+
+	# should be None
+	G = {}
+	print question3(G)
 	
-	# should print {'A': [('E', 1)], 'C': [('B', 5)], 'B': [('D', 4)], 'E': [('C', 3)]}
+	# should print {'A': [('B', 2), ('E', 1)], 'B': [('D', 4), ('A', 2)], 'E': [('A', 1), ('C', 3)]}
 	G = {'A': [('B', 2), ('E', 1)],
 		 'B': [('A', 2), ('C', 5), ('D', 4)], 
 		 'C': [('B', 5), ('D', 6), ('E', 3)],
